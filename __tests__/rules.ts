9823c850618ddefc657fac71a97dcf4ec1fea12a
Doma <leishenghao@126.com>
2020-11-07 21:34:35 +0800

    test: add tests for validation rules

--- __tests__/rules.ts
+++ __tests__/rules.ts
@@ -2,7 +2,7 @@ import { Validator } from '../src';
 
 describe('required', () => {
     test('Should fail if value is not present or has a non-null value.', () => {
-        const result = Validator.check({
+        const result: any = Validator.check({
             present: 'and non-null',
             nullish: null
         }, {
@@ -29,7 +29,7 @@ describe('required', () => {
 
 describe('size:value', () => {
     test('Should fail if string length does not match', () => {
-        const result = Validator.check({
+        const result: any = Validator.check({
             match: 'abcde',
             shorter: 'abcd',
             longer: 'abcdef'
@@ -55,7 +55,7 @@ describe('size:value', () => {
     })
 
     test('Should fail if number value does not match', () => {
-        const result = Validator.check({
+        const result: any = Validator.check({
             match: 5,
             smaller: 4,
             larger: 6
@@ -81,7 +81,7 @@ describe('size:value', () => {
     })
 
     test('Should fail if array length does not match', () => {
-        const result = Validator.check({
+        const result: any = Validator.check({
             match: [1, 2, 3, 4, 5],
             shorter: [1, 2, 3, 4],
             longer: [1, 2, 3, 4, 5, 6]
@@ -109,7 +109,7 @@ describe('size:value', () => {
 
 describe('max:value', () => {
     test('Should fail if string length is larger than {value}', () => {
-        const result = Validator.check({
+        const result: any = Validator.check({
             exact: 'abcde',
             shorter: 'abcd',
             longer: 'abcdef'
@@ -134,7 +134,7 @@ describe('max:value', () => {
     })
 
     test('Should fail if number is larger than {value}', () => {
-        const result = Validator.check({
+        const result: any = Validator.check({
             exact: 5,
             smaller: 4,
             larger: 6
@@ -159,7 +159,7 @@ describe('max:value', () => {
     })
 
     test('Should fail if array length is larger than {value}', () => {
-        const result = Validator.check({
+        const result: any = Validator.check({
             exact: [1, 2, 3, 4, 5],
             shorter: [1, 2, 3, 4],
             longer: [1, 2, 3, 4, 5, 6]
@@ -186,7 +186,7 @@ describe('max:value', () => {
 
 describe('min:value', () => {
     test('Should fail if string length is smaller than {value}', () => {
-        const result = Validator.check({
+        const result: any = Validator.check({
             exact: 'abcde',
             shorter: 'abcd',
             longer: 'abcdef'
@@ -211,7 +211,7 @@ describe('min:value', () => {
     })
 
     test('Should fail if number is smaller than {value}', () => {
-        const result = Validator.check({
+        const result: any = Validator.check({
             exact: 5,
             smaller: 4,
             larger: 6
@@ -236,7 +236,7 @@ describe('min:value', () => {
     })
 
     test('Should fail if array length is smaller than {value}', () => {
-        const result = Validator.check({
+        const result: any = Validator.check({
             exact: [1, 2, 3, 4, 5],
             shorter: [1, 2, 3, 4],
             longer: [1, 2, 3, 4, 5, 6]
@@ -257,6 +257,236 @@ describe('min:value', () => {
 
         expect(result.longer).toEqual({
             hasError: false,
+        })
+    })
+})
+
+describe('between:min,max', () => {
+    test('Should fail if string length is out of range {min}-{max}', () => {
+        const result: any = Validator.check({
+            exact: 'abcde',
+            shorter: 'abc',
+            longer: 'abcdefg'
+        }, {
+            exact: 'between:4,6',
+            shorter: 'between:4,6',
+            longer: 'between:4,6'
+        });
+
+        expect(result.exact).toEqual({
+            hasError: false,
+        })
+
+        expect(result.shorter).toEqual({
+            hasError: true,
+            errorMessage: 'The shorter must be between 4 and 6 characters.'
+        })
+
+        expect(result.longer).toEqual({
+            hasError: true,
+            errorMessage: 'The longer must be between 4 and 6 characters.'
+        })
+    })
+
+    test('Should fail if number is out of range {min}-{max}', () => {
+        const result: any = Validator.check({
+            exact: 5,
+            smaller: 3,
+            larger: 7
+        }, {
+            exact: 'number|between:4,6',
+            smaller: 'number|between:4,6',
+            larger: 'number|between:4,6'
+        });
+
+        expect(result.exact).toEqual({
+            hasError: false,
+        })
+
+        expect(result.smaller).toEqual({
+            hasError: true,
+            errorMessage: 'The smaller must be between 4 and 6.'
+        })
+
+        expect(result.larger).toEqual({
+            hasError: true,
+            errorMessage: 'The larger must be between 4 and 6.'
+        })
+    })
+
+    test('Should fail if array length is out of range {min}-{max}', () => {
+        const result: any = Validator.check({
+            exact: [1, 2, 3, 4, 5],
+            shorter: [1, 2, 3],
+            longer: [1, 2, 3, 4, 5, 6, 7]
+        }, {
+            exact: 'array|between:4,6',
+            shorter: 'array|between:4,6',
+            longer: 'array|between:4,6'
+        });
+
+        expect(result.exact).toEqual({
+            hasError: false,
+        })
+
+        expect(result.shorter).toEqual({
+            hasError: true,
+            errorMessage: 'The shorter must have between 4 and 6 items.'
+        })
+
+        expect(result.longer).toEqual({
+            hasError: true,
+            errorMessage: 'The longer must have between 4 and 6 items.'
+        })
+    })
+})
+
+describe.skip('same:other', () => {
+    test('to be done', () => {
+        // todo
+    })
+})
+
+describe.skip('different:other', () => {
+    test('to be done', () => {
+        // todo
+    })
+})
+
+describe('in:value1,value2...', () => {
+    test('Should fail if value is not included in given list', () => {
+        const result: any = Validator.check({
+            included: 'A',
+            excluded: 'E'
+        }, {
+            included: 'in:A,B,C',
+            excluded: 'in:A,B,C'
+        })
+
+        expect(result.included).toEqual({
+            hasError: false
+        })
+        expect(result.excluded).toEqual({
+            hasError: true,
+            errorMessage: 'The selected excluded is invalid.'
+        })
+    })
+})
+
+describe('notIn:value1,value2...', () => {
+    test('Should fail if value is included in given list', () => {
+        const result: any = Validator.check({
+            included: 'A',
+            excluded: 'E'
+        }, {
+            included: 'notIn:A,B,C',
+            excluded: 'notIn:A,B,C'
+        })
+
+        expect(result.included).toEqual({
+            hasError: true,
+            errorMessage: 'The selected included is invalid.'
+        })
+        expect(result.excluded).toEqual({
+            hasError: false,
+        })
+    })
+})
+
+describe('email', () => {
+    test('Should fail if value is not formatted as an e-mail address', () => {
+        const result: any = Validator.check({
+            email: 'john@example.com',
+            string: 'any other string'
+        }, {
+            email: 'email',
+            string: 'email'
+        })
+        expect(result.email).toEqual({
+            hasError: false
+        })
+        expect(result.string).toEqual({
+            hasError: true,
+            errorMessage: 'The string must be a valid email address.'
+        })
+    })
+})
+
+describe('url', () => {
+    test('Should fail if value is not a valid URL', () => {
+        const result: any = Validator.check({
+            url: 'https://google.com',
+            string: 'any other string'
+        }, {
+            url: 'url',
+            string: 'url'
+        })
+        expect(result.url).toEqual({
+            hasError: false
+        })
+        expect(result.string).toEqual({
+            hasError: true,
+            errorMessage: 'The string format is invalid.'
+        })
+    })
+})
+
+describe('unique:by?', () => {
+    test('Should fail if array contains duplicated items', () => {
+        const result: any = Validator.check({
+            distinct: [1, 2, 3, 4, 5],
+            duplicated: [1, 2, 3, 4, 5, 5]
+        }, {
+            distinct: 'array|unique',
+            duplicated: 'array|unique',
+        })
+        expect(result.distinct).toEqual({
+            hasError: false
+        })
+        expect(result.duplicated).toEqual({
+            hasError: true,
+            errorMessage: 'The duplicated field has duplicate items.'
+        })
+    })
+    test('Should fail if array contains duplicated items by property', () => {
+        const result: any = Validator.check({
+            distinct: [{ id: 1 }, { id: 2 }],
+            duplicatedById: [{ id: 1, name: 'Alice' }, { id: 1, name: 'Bob' }],
+            duplicatedByName: [{ id: 1, name: 'Tom' }, { id: 2, name: 'Tom' }]
+        }, {
+            distinct: 'array|unique:id',
+            duplicatedById: 'array|unique:id',
+            duplicatedByName: 'array|unique:id',
+        })
+        expect(result.distinct).toEqual({
+            hasError: false
+        })
+        expect(result.duplicatedById).toEqual({
+            hasError: true,
+            errorMessage: 'The duplicatedById field has duplicate items.'
+        })
+        expect(result.duplicatedByName).toEqual({
+            hasError: false,
+        })
+    })
+})
+
+describe('integer', () => {
+    test('Should fail if value is not integer', () => {
+        const result: any = Validator.check({
+            integer: 1,
+            float: 1.2
+        }, {
+            integer: 'number|integer',
+            float: 'number|integer'
+        })
+
+        expect(result.integer).toEqual({
+            hasError: false
+        })
+        expect(result.float).toEqual({
+            hasError: true,
+            errorMessage: 'The float must be an integer.'
         })
     })
 })
